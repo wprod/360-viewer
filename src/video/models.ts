eb742dd9672107bc34467d3347e8e6d75ada4e58
wittersa <amand.witters@wizbii.com>
2020-02-15 14:15:35 +0100

    Add dynamic models

--- src/video/models.ts
+++ src/video/models.ts
@@ -1,3 +1,28 @@
+export interface View<T extends ViewContentTypeEnum> {
+  name: string;
+  description: string;
+  pointOfInterest?: PointOfInterest[];
+  content: { type: T } & ViewContentByType[T];
+}
+
+export enum ViewContentTypeEnum {
+  Image = "Image",
+  Video = "Video"
+}
+
+export interface ViewContentByType {
+  [ViewContentTypeEnum.Image]: ImageType;
+  [ViewContentTypeEnum.Video]: VideoType;
+}
+
+export interface ImageType {
+  src: string;
+}
+
+export interface VideoType {
+  src: string;
+}
+
 export interface PointOfInterest {
   position: {
     x: number;
